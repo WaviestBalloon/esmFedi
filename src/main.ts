f6a1bf16146bd88013ef91730708fabb057e0be0
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2023-06-16 22:15:26 +0100

    help

--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,22 @@
 import "dotenv/config";
 import WebSocket from "ws";
-global.WebSocket = WebSocket;
-import * as Misskey from "misskey-js";
+if (!process.env.MISSKEY_TOKEN || !process.env.INSTANCE_HOST) throw new Error("MISSKEY_TOKEN or INSTANCE_HOST is not defined");
 
-if (!process.env.MISSKEY_TOKEN) throw new Error("MISSKEY_TOKEN is not defined");
+const ws = new WebSocket(`wss://${process.env.INSTANCE_HOST}/streaming?i=${process.env.MISSKEY_TOKEN}`);
+ws.once("open", () => {
+	console.log("connected");
+	ws.send(JSON.stringify({
+		type: "connect",
+		body: {
+			channel: "homeTimeline", //homeTimeline
+			id: "homeTimeline"
+		}
+	}));
+});
+ws.once("closed", () => { console.log("disconnected"); });
+ws.on("message", (data: Buffer) => {
+	const json = JSON.parse(Buffer.from(data as any, "base64").toString("utf-8"));
+	if (!json) return;
 
-const stream = new Misskey.Stream("https://misskey.test", { token: process.env.MISSKEY_TOKEN }, { WebSocket: WebSocket });
-const mainChannel = stream.useChannel("main");
-// done!
-mainChannel.on("notification", notification => {
-	console.log("notification received", notification);
-	if (notification.type !== "mention") return;
-	if ("isBot" in notification.user && notification.user.isBot) return;
-	
-	console.log("mention received", notification);
-	// type: "mention";
-	// user: User;
-	// userId: User["id"];
-	// note: Note;
-	
-	/* Note typings
-		id: ID;
-		createdAt: DateString;
-		text: string | null;
-		cw: string | null;
-		user: User;
-		userId: User['id'];
-		reply?: Note;
-		replyId: Note['id'];
-		renote?: Note;
-		renoteId: Note['id'];
-		files: DriveFile[];
-		fileIds: DriveFile['id'][];
-		visibility: 'public' | 'home' | 'followers' | 'specified'; <- I think the reply should be the same visibility as the mention
-		visibleUserIds?: User['id'][]; // Also maybe we should ask fedi about how it should behave with replies in general
-		localOnly?: boolean;
-		myReaction?: string;
-		reactions: Record<string, number>;
-		renoteCount: number;
-		repliesCount: number;
-		poll?: {
-			expiresAt: DateString | null;
-			multiple: boolean;
-			choices: {
-				isVoted: boolean;
-				text: string;
-				votes: number;
-			}[];
-		};
-		emojis: {
-			name: string;
-			url: string;
-		}[];
-		uri?: string;
-		url?: string;
-		isHidden?: boolean;
-	*/
-	// there is no "type" in package.json okay	
-	
-
-
-
-
-
-	
+	console.log(json);
 });
